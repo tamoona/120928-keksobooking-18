949d2330f1f551f752d350a0b3a2b5aac7dd894b
Anastasiia <nanitamoonaboo@gmail.com>
2019-11-15 20:46:43 +0300

    Remove unused functions

--- js/util.js
+++ js/util.js
@@ -1,31 +1,6 @@
 'use strict';
 
 (function () {
-// cлучайное число диапазона
-  var getRandomInteger = function (min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  };
-
-  // генерация массива случайной длины на основе массива
-  var getRandomArrayFromArray = function (array) {
-    var lastElementIndex = array.length - 1;
-    var randomArrayStartIndex = getRandomInteger(0, lastElementIndex);
-    var randomArrayEndIndex = getRandomInteger(randomArrayStartIndex, lastElementIndex);
-
-    if (randomArrayStartIndex === randomArrayEndIndex) {
-      return array.slice(randomArrayStartIndex);
-    }
-
-    return array.slice(randomArrayStartIndex, randomArrayEndIndex);
-  };
-
-  // генерация случайного элемента из массива
-  var getRandomValueFromArray = function (array) {
-    var index = getRandomInteger(0, array.length - 1);
-    return array[index];
-  };
-
   // функция, добавляющая значение в поле
   var setFieldValue = function (fieldElement, value) {
     fieldElement.value = value;
@@ -97,9 +72,6 @@
   };
 
   window.utils = {
-    getRandomInteger: getRandomInteger,
-    getRandomArrayFromArray: getRandomArrayFromArray,
-    getRandomValueFromArray: getRandomValueFromArray,
     setFieldValue: setFieldValue,
     removeElement: removeElement,
     removeElements: removeElements,
